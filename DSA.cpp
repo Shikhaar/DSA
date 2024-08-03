b69e828e8e25c11b646401c9954e5368b3a7e6f7
Shikhar <shikharsrivastava3004@gmail.com>
2024-08-03 16:58:22 +0530

    Done some capitilization

--- DSA.cpp
+++ DSA.cpp
@@ -5,9 +5,9 @@ int main(){
     int num = 2;
     cout<<endl;
     switch(num){
-        case 1: cout<< "first" << endl;
+        case 1: cout<< "First" << endl;
             break;
-        case 2: cout<<"second" << endl;
+        case 2: cout<<"Second" << endl;
             break;
         default: cout<<"it is a default case" << endl;
 

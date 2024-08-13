ed5a9816b0413172420c17f3dc381cfa8ed2aca1
Shikhar <shikharsrivastava3004@gmail.com>
2024-08-13 13:15:00 +0530

    reversing the array

--- DSA.cpp
+++ DSA.cpp
@@ -15,6 +15,239 @@ int main(){
 
     }
     return 0;
+    #include<iostream>
+using namespace std;
+int main() 
+/*{ int a=55;
+ cout<<a<<endl;
+char A = 'a';
+ cout<<A<<endl;
+
+  int a = 'a';
+  cout<<a<<endl; 
+  int a = 5;
+  int b =7;
+  if (a>b)
+  cout<<"a is bigger"<<endl;
+ else
+cout<<"b is bigger"<<endl; 
+
+  int a;
+  cin>> a;
+  if (a>0)
+    cout<<"a is positive"<<endl;
+  else
+    cout<<"a is negative"<<endl;
+
+
+  int n;
+  cin>>n;
+  int i=1;
+  while(i<=n){
+    cout<<i<<" ";
+    i=i+2;
+  }
+
+
+      int n;
+      cin >> n;
+      int i = 1;
+
+      while (i <= n) {
+          int j = 1;
+          while (j <= n) {
+              cout << "*";;
+              j = j + 1;
+          }
+          cout << endl;
+          i = i + 1; 
+
+       int n;
+       cin>>n;
+       int i=1;
+       while(i<=n){
+           int j=1;
+           while(j<=n){
+               cout<<n-j+1;
+               j=j+1;
+           }
+           cout<<endl;
+           i=i+1;
+       }
+       
+
+ int n;
+  cin>>n;
+  int i=1;
+  int count = 1;
+  while(i<=n){
+    int j=1;
+    while(j<=n){
+     
+      cout<<count<<" ";
+      count=count+1;
+      j=j+1;
+      
+    }
+    cout<<endl;
+    i=i+1;
+  }
+         
+
+  int n;
+   cin>>n;
+
+   int row=1;
+   while(row<=n){
+       int col=1;
+       while(col<=row){
+           cout<<col;
+           col=col+1;
+
+       }
+       cout<<endl;
+       row=row+1;
+   }
+
+
+
+  int n;
+  cin>>n;
+  int row=1;
+  while(row<=n){
+    int col =1;
+    int value =row;
+    while(col<=row){
+      cout<<value;
+      value=value+1;
+      col=col+1; 
+    }
+    cout<<endl;
+    row=row+1;
+  }
+
+
+int n;
+cin>>n;
+
+  int row=1;
+  while(row<=n){
+    int col=1;
+    while(col<=n){
+      char ch ='A' + row -1;
+      cout<<ch;
+      col=col+1;
+    }
+    cout<<endl;
+    row=row+1;
+  }
+  
+  
+
+  int n;
+  cin>> n;
+   int i=1;
+  while (i<=n){
+    int j=1;
+    while( j<=i){
+      char ch=('A'+i+j-2)  ; 
+      cout<<ch<<" ";
+       j=j+1;
+    }
+    cout<<endl;
+    i=i+1;
+  }
+
+
+  int n;
+  cin>>n;
+  int i=1;
+  while(i<=n){
+    int space =n-i;
+    while(space){
+      cout<<" ";
+      space=space-1;
+    }
+    int j=1;
+    while(j<=i){
+      cout<<'*';
+      j=j+1;
+    }
+    cout<<endl;
+    i=i+1;
+  }
+  
+ 
+int n;
+cin>>n;
+  int i=1;
+  while(i<=n){
+    int space=n-i;
+    while(space){
+      cout<<" ";
+      space=space-1;
+    }
+    int j=1;
+    while(j<=i){
+      cout<<j;
+      j=j+1;
+    }
+    int start=i-1;
+    while(start){
+      cout<<start;
+      start=start-1;
+    }
+    cout<<endl;
+    i=i+1;
+  }
+
+  int a=4;
+  int b=6;
+  cout<<" a&b "<<(a&b)<<endl;
+  cout<<" a|b "<<(a|b)<<endl;
+  cout<<" ~a "<<(~a)<<endl;
+  cout<< " a^b "<<(a^b)<<endl;
+
+        
+cout<< (21>>1)<<endl;
+cout<<(21>>2)<<endl;
+       
+int n;
+cout<<"until which number you want to print"<<endl;
+  cin>>n;
+  int a=0;
+  int b=1;                     //fibonacii
+  for(int i=1;i<=n;i++){            
+    int nextno=a+b;
+    cout<<nextno<< " ";
+    a=b;
+    b=nextno;
+    
+  }
+         
+
+int n;
+cout<<"enter a number"<<" ";
+cin>>n;
+bool isPrime =1;
+for(int i=2;i<n;i++){
+  if(n%i==0){
+    
+  }
+  
+}
+  
+        */
+
+
+  
+}
+
+
+  
+
+  
+
 
 
 

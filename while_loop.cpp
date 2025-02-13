59dc8eef3ab624df3c6a0cb6f65e0d777e30a166
Shikhaar <shikharsrivastava3004@gmail.com>
2025-02-13 11:47:43 +0530

    while_loop

---
+++ while_loop.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){                
+    int n;
+    cin >> n;
+    int i = 10;
+    while(i>=1){
+        cout<< (i * n) << " ";
+        i--;
+    }
+    cout << endl;
+    return 0;
+}

993372271acd160a1c74017ff74aa0b28d4dad1b
Shikhaar <shikharsrivastava3004@gmail.com>
2025-03-09 22:36:03 +0530

    friend function

---
+++ friend function.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+class Complex {
+private:
+    int real, imag;
+
+public:
+    Complex(int r = 0, int i = 0) : real(r), imag(i) {}
+
+    friend Complex operator+(const Complex &c1, const Complex &c2);
+
+    void display() {
+        cout << real << " + " << imag << "i" << endl;
+    }
+};
+
+Complex operator+(const Complex &c1, const Complex &c2) {
+    return Complex(c1.real + c2.real, c1.imag + c2.imag);
+}
+
+int main() {
+    Complex c1(3, 4), c2(1, 2);
+    Complex c3 = c1 + c2;  
+
+    cout << "Result of addition: ";
+    c3.display();  
+
+    return 0;
+}

9e1d71f5dd071ceb9868353a2be8c559e33e40d8
Shikhaar <shikharsrivastava3004@gmail.com>
2025-03-29 16:40:15 +0530

    void func

---
+++ void func.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+ 
+void printname(string name) {
+    cout << "hey! " << name << endl;
+}
+
+int main() {
+    string name;
+    cin >> name;
+    printname(name);
+    return 0;
+}
